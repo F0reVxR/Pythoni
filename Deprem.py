5209995746052f9150010dae2b0fab2291f588a0
F0reVxR <f1n2l0v3_o6e9@tutamail.com>
2025-09-18 20:04:30 +0300

    major deprem increase

--- Deprem.py
+++ Deprem.py
@@ -1,2 +1,12 @@
 
-print('Hi!', input('Deprem? (yes/yes)'), 'take -1000$')
+#print('Hi!', input('Deprem? (yes/yes)'), 'take -1000$')
+
+"""
+101 
+102 
+103
+
+"""
+x = (input('Введите число '))
+print("Ваш депрем составляет", x, "$")
+
